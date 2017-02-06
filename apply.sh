3893151fc74ea63a93f9b494320127b7023cbc71
sooorajjj <sooorajjj@gmail.com>
2017-02-07 00:39:06 +0530

    Show the output and dont exit !

--- apply.sh
+++ apply.sh
@@ -2,16 +2,27 @@
 
 MYABSPATH=$(readlink -f "$0")
 PATCHBASE=$(dirname "$MYABSPATH")
-CMBASE=$(readlink -f "$PATCHBASE/../../")
+AOSPBASE=$(readlink -f "$PATCHBASE/../")
 
 for i in $(find "$PATCHBASE"/* -type d); do
 	PATCHNAME=$(basename "$i")
 	PATCHTARGET=$PATCHNAME
 	for i in $(seq 4); do
 		PATCHTARGET=$(echo $PATCHTARGET | sed 's/_/\//')
-		if [ -d "$CMBASE/$PATCHTARGET" ]; then break; fi
+		if [ -d "$AOSPBASE/$PATCHTARGET" ]; then break; fi
 	done
 	echo applying $PATCHNAME to $PATCHTARGET
-	cd "$CMBASE/$PATCHTARGET" || exit 1
-	git am -3 "$PATCHBASE/$PATCHNAME"/* || exit 1
+	cd "$AOSPBASE/$PATCHTARGET"
+	git am "$PATCHBASE/$PATCHNAME"/*
+	# CMD_OUTPUT=$(git am -3 "$PATCHBASE/$PATCHNAME"/*)
+	# echo -e $CMD_OUTPUT\n\n
+
+	# if [[ $CMD_OUTPUT =~ échoué.|error.|fail. ]]; then
+	# 	git am --abort
+	# 	echo Ran into an error
+	# else
+	# 	echo Patch applied !
+	# fi
+
+	cd "$PATCHBASE"
 done
